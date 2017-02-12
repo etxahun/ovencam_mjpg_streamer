c5b03b3ea800f0197009b4f0ba4f485f6a111927
Etxahun Sanchez Bazterretxea <etxahun.sanchez@gmail.com>
2017-02-12 23:17:43 +0100

    Update camera_start.sh

--- mjpg-streamer/camera_start.sh
+++ mjpg-streamer/camera_start.sh
@@ -20,4 +20,4 @@ echo -n 1 > "$ovencampath/public/resources/estado_mjpgstreamer.txt"
 cd /usr/src/mjpg-streamer/mjpg-streamer-experimental/
 export LD_LIBRARY_PATH=.
 # ./mjpg_streamer -o "output_http.so -w /home/pi/oven_cam/prueba/camera/www -p 8081" -i "input_raspicam.so -rot 270 -fps 15 -q 50 -x 640 -y 480 ex night"
-./mjpg_streamer -o "output_http.so -w $ovencampath/mjpg-streamer/www -p 8081 -c etxea:etxean3sb" -i "input_raspicam.so -fps 25 -q 50 -x 320 -y 240 ex night"
+./mjpg_streamer -o "output_http.so -w $ovencampath/mjpg-streamer/www -p 8081 -c <user>:<passwd>" -i "input_raspicam.so -fps 25 -q 50 -x 320 -y 240 ex night"
