c5254a2999618ef0322c3f8c4bc219b0affe06cb
Etxahun Sanchez Bazterretxea <etxahun.sanchez@gmail.com>
2017-04-17 01:53:24 +0200

    Update camera_stop.sh
    Minor fix on initial "estado_mjpgstreamer.txt" establishment.

--- mjpg-streamer/camera_stop.sh
+++ mjpg-streamer/camera_stop.sh
@@ -3,11 +3,12 @@
 # Cargamos el fichero de configuración:
 . project_path.cfg
 
+# Guardar el estado:
+echo -n 0 > "$ovencampath/public/resources/estado_mjpgstreamer.txt"
+
 if pgrep mjpg_streamer
 then
-  # Guardar el estado:
-  echo -n 0 > "$ovencampath/public/resources/estado_mjpgstreamer.txt"
-
+  # Matamos el proceso MJPG_STREAMER
   kill $(pgrep mjpg_streamer) > /dev/null 2>&1
   echo "mjpg_streamer stopped"
 else
